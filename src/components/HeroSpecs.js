5c1c14fe554135037e115dd21300b94d4faad1c3
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-08-03 13:28:05 -0600

    .

--- src/components/HeroSpecs.js
+++ src/components/HeroSpecs.js
@@ -7,60 +7,60 @@ import speedIcon from "../assets/speed.png";
 import strengthIcon from "../assets/strength.png";
 
 function HeroSpecs({ showHeroSpecs, featuredHero, onGoBack }) {
-  return (
-    <div id="image-container">
-      <h4 id="titles">{featuredHero.name}</h4>
-      <div id="featuredHero-powerstats">
-        <ul>
-          <li>
-            {" "}
-            <img
-              className="stats-icons"
-              src={intelligenceIcon}
-              alt="intelligence"
-            />{" "}
-            {featuredHero.powerstats.intelligence}{" "}
-          </li>
-          <li>
-            {" "}
-            <img
-              className="stats-icons"
-              src={strengthIcon}
-              alt="strength"
-            />{" "}
-            {featuredHero.powerstats.strength}{" "}
-          </li>
-          <li>
-            {" "}
-            <img className="stats-icons" src={speedIcon} alt="speed" />{" "}
-            {featuredHero.powerstats.speed}{" "}
-          </li>
-          <li>
-            {" "}
-            <img
-              className="stats-icons"
-              src={durabilityIcon}
-              alt="durability"
-            />{" "}
-            {featuredHero.powerstats.durability}{" "}
-          </li>
-          <li>
-            {" "}
-            <img className="stats-icons" src={powerIcon} alt="power" />{" "}
-            {featuredHero.powerstats.power}{" "}
-          </li>
-          <li>
-            {" "}
-            <img className="stats-icons" src={combatIcon} alt="combat" />{" "}
-            {featuredHero.powerstats.combat}{" "}
-          </li>
-        </ul>
-      </div>
-      <img src={featuredHero.images.lg} alt="" id="images"></img>
-      <button>Add to Favorites</button>
-      <button onClick={onGoBack}>Back to Collection</button>
-    </div>
-  );
+    return (
+        <div id="image-container">
+            <h4 id="titles">{featuredHero.name}</h4>
+            <div id="featuredHero-powerstats">
+                <ul>
+                    <li>
+                        {" "}
+                        <img
+                            className="stats-icons"
+                            src={intelligenceIcon}
+                            alt="intelligence"
+                        />{" "}
+                        {featuredHero.powerstats.intelligence}{" "}
+                    </li>
+                    <li>
+                        {" "}
+                        <img
+                            className="stats-icons"
+                            src={strengthIcon}
+                            alt="strength"
+                        />{" "}
+                        {featuredHero.powerstats.strength}{" "}
+                    </li>
+                    <li>
+                        {" "}
+                        <img className="stats-icons" src={speedIcon} alt="speed" />{" "}
+                        {featuredHero.powerstats.speed}{" "}
+                    </li>
+                    <li>
+                        {" "}
+                        <img
+                            className="stats-icons"
+                            src={durabilityIcon}
+                            alt="durability"
+                        />{" "}
+                        {featuredHero.powerstats.durability}{" "}
+                    </li>
+                    <li>
+                        {" "}
+                        <img className="stats-icons" src={powerIcon} alt="power" />{" "}
+                        {featuredHero.powerstats.power}{" "}
+                    </li>
+                    <li>
+                        {" "}
+                        <img className="stats-icons" src={combatIcon} alt="combat" />{" "}
+                        {featuredHero.powerstats.combat}{" "}
+                    </li>
+                </ul>
+            </div>
+            <img src={featuredHero.images.lg} alt="" id="images"></img>
+            <button>Add to Favorites</button>
+            <button onClick={onGoBack}>Back to Collection</button>
+        </div>
+    );
 }
 
 export default HeroSpecs;
