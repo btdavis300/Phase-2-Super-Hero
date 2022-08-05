ce39b013ea0610dc54065e88639f7bb3e5c05b9f
Brennan Davis <brennantd@gmail.com>
2022-08-05 10:07:34 -0600

    fixed MYO css page

--- src/components/MakeYourOwn.js
+++ src/components/MakeYourOwn.js
@@ -49,70 +49,102 @@ function MakeYourOwn({ onAddHero, MYOHeroes, onFavoriteHero, onMYOHeroes }) {
   }
 
   return (
-    <div className="new-bot-form">
-      <h2 id="create-your-own">- Create Your Own Hero! -</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          name="image"
-          placeholder="Image"
-          value={sm}
-          onChange={(e) => setSm(e.target.value)}
-        />
-        <input
-          type="text"
-          name="intelligence"
-          placeholder="Intelligence"
-          value={intelligence}
-          onChange={(e) => setIntelligence(e.target.value)}
-        />
-        <input
-          type="text"
-          name="strength"
-          placeholder="Strength"
-          value={strength}
-          onChange={(e) => setStrength(e.target.value)}
-        />
-        <input
-          type="text"
-          name="speed"
-          placeholder="Speed"
-          value={speed}
-          onChange={(e) => setSpeed(e.target.value)}
-        />
-        <input
-          type="text"
-          name="durability"
-          placeholder="Durability"
-          value={durability}
-          onChange={(e) => setDurability(e.target.value)}
-        />
-        <input
-          type="text"
-          name="power"
-          placeholder="Power"
-          value={power}
-          onChange={(e) => setPower(e.target.value)}
-        />
-        <input
-          type="text"
-          name="combat"
-          placeholder="Combat"
-          value={combat}
-          onChange={(e) => setCombat(e.target.value)}
-        />
-        <button id="submit-button" type="submit">
-          Add New Hero
-        </button>
+    <div>
+      <div id="new-bot-form">
+        <h1 className="page-title">MAKE YOUR OWN HERO</h1>
+        <form onSubmit={handleSubmit}>
+          <div id="name-image-form">
+            <section className="input-box">
+              <label>Name</label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </section>
+            <section className="input-box">
+              <label>Image</label>
+              <input
+                type="text"
+                name="image"
+                placeholder="Image"
+                value={sm}
+                onChange={(e) => setSm(e.target.value)}
+              />
+            </section>
+          </div>
+          <div id="first-three-specs-form">
+            <section className="input-box">
+              <label>Intelligence</label>
+              <input
+                type="text"
+                name="intelligence"
+                placeholder="Intelligence"
+                value={intelligence}
+                onChange={(e) => setIntelligence(e.target.value)}
+              />
+            </section>
+            <section className="input-box">
+              <label>Strength</label>
+              <input
+                type="text"
+                name="strength"
+                placeholder="Strength"
+                value={strength}
+                onChange={(e) => setStrength(e.target.value)}
+              />
+            </section>
+            <section className="input-box">
+              <label>Speed</label>
+              <input
+                type="text"
+                name="speed"
+                placeholder="Speed"
+                value={speed}
+                onChange={(e) => setSpeed(e.target.value)}
+              />
+            </section>
+          </div>
+          <div id="second-three-specs-form">
+            <section className="input-box">
+              <label>Durability</label>
+              <input
+                type="text"
+                name="durability"
+                placeholder="Durability"
+                value={durability}
+                onChange={(e) => setDurability(e.target.value)}
+              />
+            </section>
+            <section className="input-box">
+              <label>Power</label>
+              <input
+                type="text"
+                name="power"
+                placeholder="Power"
+                value={power}
+                onChange={(e) => setPower(e.target.value)}
+              />
+            </section>
+            <section className="input-box">
+              <label>Combat</label>
+              <input
+                type="text"
+                name="combat"
+                placeholder="Combat"
+                value={combat}
+                onChange={(e) => setCombat(e.target.value)}
+              />
+            </section>
+          </div>
+          <button id="submit-button" type="submit">
+            Add New Hero
+          </button>
 
-      </form>
+        </form>
+      </div>
       <div id="image-field">{displayMYOHeroes}</div>
     </div>
   );
