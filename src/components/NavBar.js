6a24b905fd66aa217140eb2c4e9c8b7386d7e1d2
Brennan Davis <brennantd@gmail.com>
2022-08-05 06:43:49 -0600

    when Home is clicked, it resets state to show all hero cards

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import Search from "./Search";
 const NavBar = ({ onSearchChange, searchTerm, onGoBack }) => {
   return (
     <div className="topnav">
-      <Link to="/">Home</Link>
+      <Link onClick={onGoBack} to="/">Home</Link>
       <Link to="/favorites">Favorites</Link>
       <Link to="/sortby">Sort By</Link>
       <Link to="/MYO">Make Your Own</Link>
